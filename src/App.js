a6c00052f731b839e8ece1acc67516103f8d6c30
Vara <prasadnti@gmail.com>
2023-09-09 15:05:57 +0530

    test UI

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ const api = {
 
 function App() {
 
-  const [search, setSearch] = useState("")
+  const [search, setSearch] = useState(" ")
   const [location, setLocation] = useState({})
   const [weather, setWeather] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(false)
 
   // console.log("Weather", weather)
 
@@ -28,6 +28,7 @@ function App() {
       .then((res) => res.json())
       .then((result) => { 
         const daywiselist = dateFilter(result)
+        console.log(daywiselist)
         const daywise = fiveDaysData(daywiselist)
         setWeather(daywise);
     });
@@ -39,10 +40,13 @@ function App() {
   }, [location])
 
   const searchPressed = () => {
+    setIsLoading(true)
     fetch(`${api.base}direct?q=${search}&limit=${api.limit}&appid=${api.key}`)
       .then((res) => res.json())
       .then((result) => {
+       
       setLocation(result[0]);
+      setIsLoading(false)
       });
   };
 
@@ -67,16 +71,17 @@ function App() {
   }
 
   return (
-    <div className="App">
+    <div className="container">
       <h1>Weather in you city</h1>
-      <div>
+      <div className='search'>
         <input type='text' value={search} onChange={(e) => searchHandler(e.target.value)}/>
-        <button onClick={() => searchPressed()}>Search</button> {isLoading ? 
+        <button onClick={() => searchPressed()}>Search</button> <br />
+        {isLoading ? 
         <i className="fa fa-spinner" aria-hidden="true"></i> : null}
-      </div>
-      <div>
-      </div>
+      <div className='tableData'>
       <TableComponent weather={weather} />
+      </div>
+      </div>
     </div>
   );
 }
--- src/tableComponent.js
+++ src/tableComponent.js
@@ -5,39 +5,41 @@ const tableComponent = ({weather}) => {
     weather.map(item => {
         return (
             <table cellPadding="0" cellSpacing="0" key={item.dt}>
-        <thead>
-            <tr>
-            <th colSpan={2} className='date'>{item.dt_txt.split(" ")[0]}</th>
-            </tr>
-        </thead>
-        <tbody>
-        <tr>
-          <td colSpan={2} className='temp'>{item.main.temp}</td>
-        </tr>
-        <tr>
-          <td colSpan={2}>
-            <table cellPadding="0" cellSpacing="0">
+                <thead>
+                    <tr>
+                        <th colSpan={2} className='date'>{item.dt_txt.split(" ")[0]}</th>
+                    </tr>
+                </thead>
+                <tbody>
                 <tr>
-                  <td>Min</td>
-                  <td>Max</td>
+                <td colSpan={2} className='temp'>{item.main.temp}</td>
                 </tr>
                 <tr>
-                <td>{item.main.temp_min}</td>
-                  <td>{item.main.temp_max}</td>
+                <td colSpan={2}>
+                    <table cellPadding="0" cellSpacing="0">
+                        <tbody>
+                            <tr>
+                                <td>Min</td>
+                                <td>Max</td>
+                            </tr>
+                            <tr>
+                                <td>{item.main.temp_min}</td>
+                                <td>{item.main.temp_max}</td>
+                            </tr>
+                            <tr>
+                                <td>Pressure</td>
+                                <td>{item.main.pressure}</td>
+                            </tr>
+                            <tr>
+                                <td>Humidity</td>
+                                <td>{item.main.humidity}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
                 </tr>
-                <tr>
-                  <td>Pressure</td>
-                  <td>{item.main.pressure}</td>
-                </tr>
-                <tr>
-                  <td>Humidity</td>
-                  <td>{item.main.humidity}</td>
-                </tr>
+                </tbody>
             </table>
-          </td>
-        </tr>
-        </tbody>
-      </table>
         )
     })
     
